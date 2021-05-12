0ffdf1aab1035273eb0e3043c7168fcf212a2f55
Arfaouim <arfaouimehdi20@yahoo.fr>
2021-05-12 19:41:49 +0200

    Java Variables and Literals
    In this tutorial, we will learn about Java variables and literals with the help of examples

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args){
+        int age;
+        age = 25;
+        double x = 2.1;
+        float myFloat = 3.4F;
+        // 3.445*10^2
+        double myDoubleScientific = 3.445e2;
+        char c = 'M';
+        boolean flag1 = false;
+        String str1 = "Java Programming";
+       // System.out.println(age);
+       // System.out.println(x);
+       // System.out.println(c);
+       // System.out.println(flag1);
+       // System.out.println(myFloat);
+       // System.out.println(myDoubleScientific);
+        System.out.println(str1);
+    }
+}
