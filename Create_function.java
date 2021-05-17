a8312cfc487968dbc740b754328981471b8f4575
Arfaouim <arfaouimehdi20@yahoo.fr>
2021-05-18 00:58:48 +0200

    function with argument

--- Create_function.java
+++ Create_function.java
@@ -1,4 +1,6 @@
+// The main function should be the same name as the file !
 public class Create_function {
+
     static void myMethod() {
       System.out.println("I just got executed!");
     }
---
+++ create_function_2.java
@@ -0,0 +1,13 @@
+public class create_function_2 {
+
+    // create the method
+    static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
+      }
+    // call the method
+      public static void main(String[] args) {
+        myMethod("Liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+      }
+}
