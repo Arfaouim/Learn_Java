54fb8db94dda74249b6424b40d4a093beb7481ae
Arfaouim <arfaouimehdi20@yahoo.fr>
2021-05-19 23:36:56 +0200

    generale way to define function

---
+++ create_function_General.java
@@ -0,0 +1,9 @@
+public class create_function_General {
+    // Syntax
+    public static int methodName(int a, int b) {
+    // public static −-> modifier
+    // int −-> return type
+    // a, b −-> formal parameters
+    // int a, int b −-> list of parameters
+    }
+}
