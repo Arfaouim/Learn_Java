5020044fb7fe96b5a2244c75498cffdb986094a7
Arfaouim <arfaouimehdi20@yahoo.fr>
2021-05-19 23:26:17 +0200

    build simple function

---
+++ create_function_3.java
@@ -0,0 +1,9 @@
+public class create_function_3 {
+    
+    static void method_1(int a, int b){
+        System.out.println(a+b);
+    }
+    public static void  main(String[] args){
+        method_1(2,1);
+    }
+}
